566300b8315c58169c01b75153b39b4a6b7db16e
Abdullah <Abdullah@Abdu>
2016-08-15 20:01:49 +0530

    Second edit

--- src/DAO/DBConnector.java
+++ src/DAO/DBConnector.java
@@ -5,10 +5,32 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Abdullah
  */
 public class DBConnector {
-    
+    private Connection DBConnection;
+    public Connection connect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Connection successful");
+        }
+        catch(ClassNotFoundException cnfe){
+            System.out.println("Connection failed" + cnfe);
+        }
+        String url = "jdbc:mysql://localhost:3306/bookstore";
+        try{
+            DBConnection = (Connection) DriverManager.getConnection(url, "root", "");
+            System.out.println("Database Connected");
+        }
+        catch(SQLException se){
+            System.out.println("Database Not Found " + se);
+        }
+        return DBConnection;
+    }
 }
