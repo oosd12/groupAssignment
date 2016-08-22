401280b9eb0a24aedbf844a0c2753f6d43a67d3c
Abdullah <Abdullah@Abdu>
2016-08-22 21:58:01 +0530

    View Products functionality added

--- src/CoreClasses/Product.java
+++ src/CoreClasses/Product.java
@@ -5,10 +5,31 @@
  */
 package CoreClasses;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Abdullah
  */
 public class Product {
     
+    public ResultSet getAvailableProducts(){
+        ResultSet rs = null;
+        java.sql.Connection conn = new DBConnector().connect();
+        try{
+            String sql= "SELECT sp.product_id, sp.supplier_id, p.name,s.name, sp.quantity_available, sp.production_date, sp.price, p.category " +
+                        "FROM Supplier_Product sp " +
+                        "JOIN Supplier s on sp.supplier_id = s.supplier_id "+
+                        "JOIN Product p on sp.product_id = p.product_id";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        
+        return rs;
+    }
 }
